ce5975752361e1d5cdd6f43bc9c5609bc89ff80d
aman-a-dev <amanuelantenha@gmail.com>
2026-09-12 14:25:15 +0300

    shadcn and nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-center text-5xl mt-5">Hellow World</h1>
+    </div>
+  );
+}
